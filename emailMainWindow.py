3c95467e22bdbab15013e16aa00d268a3b5021a7
Kuramenai <marcelynsteeve@gmail.com>
2021-12-25 20:16:21 +0800

    Fifth Commit

--- emailMainWindow.py
+++ emailMainWindow.py
@@ -1,3 +1,4 @@
+from email.message import EmailMessage
 import sys
 import smtplib
 import imaplib
@@ -9,7 +10,7 @@ from PyQt5 import QtCore as qtc
 
 
 class MainWindow(qtw.QMainWindow):
-    def __init__(self,email,password):
+    def __init__(self,email,password,mail,server_SMPTP):
         "Main Window constructor"
         super().__init__()
 
@@ -20,17 +21,21 @@ class MainWindow(qtw.QMainWindow):
         self.winHeight = 900
         self.setFixedSize(self.winWidth,self.winHeight)
 
-        #Create the server
-        self.host = 'imap.gmail.com'
-        self.server = self.host
-        self.mail  = imaplib.IMAP4_SSL(self.server)
-        self.login = False
-       
+        #Servers
+        self.mail = mail
+        self.server = server_SMPTP
+
         #存储邮件账户与密码
         self.my_email = email
         self.password = password
         self.email_label = qtw.QLabel(email)
         self.password_label = qtw.QLabel(password)
+
+        self.my_messages = []
+        self.mails_list = qtw.QListWidget()
+        self.current_item = 0
+        self.selected_items = []
+        self.source2display = 0
         
         #Create the buttons for the sidebar menu
         self.new_email_button = qtw.QPushButton("New Email")
@@ -45,10 +50,11 @@ class MainWindow(qtw.QMainWindow):
         self.account_button.clicked.connect(self.btn_account_function)
 
         #Create a tab for when each sidebar button is pressed
-        self.new_email_tab = self.new_email_tab()
-        self.sent_tab = self.sent_tab()
-        self.inbox_tab  = self.inbox_tab()
-        self.account_tab = self.account_tab()
+        self.tab1 = self.new_email_tab()
+        self.tab2 = self.sent_tab()
+        self.tab3  = self.inbox_tab()
+        self.tab4 = self.account_tab()
+        self.tab5 = self.read_email_tab()
 
         self.initUI()
         #End main UI code
@@ -74,10 +80,7 @@ class MainWindow(qtw.QMainWindow):
         self.right_widget = qtw.QTabWidget()
         self.right_widget.tabBar().setObjectName("mainTab")
         #Adding tabs to the tab Widget
-        self.right_widget.addTab(self.new_email_tab, '')
-        self.right_widget.addTab(self.sent_tab, '')
-        self.right_widget.addTab(self.inbox_tab, '')
-        self.right_widget.addTab(self.account_tab, '')
+        self.update_tab()
 
        # 下面这段代码隐藏了标签部件的标签并初始化显示页面
         self.right_widget.setCurrentIndex(2)
@@ -94,7 +97,16 @@ class MainWindow(qtw.QMainWindow):
         main_widget = qtw.QWidget()
         main_widget.setLayout(main_layout)
         self.setCentralWidget(main_widget)
-    
+
+    def update_tab(self):
+        "Function that updates the tabs layout when there are new changes"
+        self.right_widget.addTab(self.tab1, '')
+        self.right_widget.addTab(self.tab2, '')
+        self.right_widget.addTab(self.tab3, '')
+        self.right_widget.addTab(self.tab4, '')
+        self.right_widget.addTab(self.tab5, '')
+
+#SideMenu Buttons   
     def btn_email_function(self):
         "Opens the new email tab"
         self.right_widget.setCurrentIndex(0)
@@ -106,19 +118,24 @@ class MainWindow(qtw.QMainWindow):
         self.right_widget.setCurrentIndex(2)
     def btn_account_function(self):
         "Opens the account tab"
-        self.right_widget.setCurrentIndex(3)
+        self.right_widget.setCurrentIndex(3)  
+   
+    def btn_read_email(self):
+        "Opens the read email tab" 
+        self.right_widget.setCurrentIndex(4)
 
-    def login_in(self):
-        "Test if user has been logging in"
-        #print('step2')
-        try:
-            if (self.login == False):
-                self.mail.login(self.my_email,self.password)
-                #print('step2"')
-                self.login = True
-        except Exception as error:
-            print(error)
-            self.login = False
+#Important Functions
+    # def login_in(self):
+    #     "Test if user has been logging in"
+    #     #print('step2')
+    #     try:
+    #         if (self.login == False):
+    #             self.mail.login(self.my_email,self.password)
+    #             #print('step2"')
+    #             self.login = True
+    #     except Exception as error:
+    #         print(error)
+    #         self.login = False
 
     def send_function(self):
         "Implement the sending function"
@@ -128,10 +145,10 @@ class MainWindow(qtw.QMainWindow):
                 return
             else: 
                 message2Send = 'Subject: {}\n\n{}'.format(self.subject.text(),self.body.toPlainText())
-                server = smtplib.SMTP('smtp.gmail.com',587)#'smtp.gmail.com',587
-                server.starttls()
-                server.login(self.my_email,self.password)
-                server.sendmail(self.my_email,self.receiver.text(),message2Send)
+                # server = smtplib.SMTP('smtp.gmail.com',587)#'smtp.gmail.com',587
+                # server.starttls()
+                # server.login(self.my_email,self.password)
+                self.server.sendmail(self.my_email,self.receiver.text(),message2Send)
                 print("Message Sent")
         except Exception as error:
             print(error)
@@ -140,63 +157,89 @@ class MainWindow(qtw.QMainWindow):
     def check_for_new_mails(self):
         "Check if there are new emails"
         #print('step3')
-        self.login_in()
-        if self.login:
-            self.mail.select('inbox')
-            _, search_data = self.mail.search(None,'UNSEEN')
-            my_messages  = []
-            for num in search_data[0].split():
-                
-                email_data = {}
+        #self.login_in()
+        #if self.login:
+        self.mail.select('inbox')
+        _, search_data = self.mail.search(None,'UNSEEN')
+    
+        for num in search_data[0].split():
+            
+            email_data = {}
 
-                _, data = self.mail.fetch(num,"RFC822")
-                _,b = data[0]
-                email_message = email.message_from_bytes(b)
-                
-                for header in ['subject','to','from','date']:
-                    #print("{}:{}".format(header,email_message[header]))
-                    email_data[header] = email_message[header]
+            _, data = self.mail.fetch(num,"RFC822")
+            _,b = data[0]
+            email_message = email.message_from_bytes(b)
+            
+            for header in ['subject','to','from','date']:
+                #print("{}:{}".format(header,email_message[header]))
+                email_data[header] = email_message[header]
 
-                for part in email_message.walk():
-                    if part.get_content_type() == 'text/plain':
-                        body = part.get_payload(decode= True)
-                        email_data[body] = body.decode()
-                        #print(body.decode())
-                    elif part.get_content_type() == 'text/html':
-                        body = part.get_payload(decode= True)
-                        email_data[body] = body.decode()
-                       # print(body.decode())
-                my_messages.append(email_data)
-            return my_messages
+            for part in email_message.walk():
+                if part.get_content_type() == 'text/plain':
+                    body = part.get_payload(decode= True)
+                    email_data['body'] = body.decode()
+                    #print(body.decode())
+                elif part.get_content_type() == 'text/html':
+                    body = part.get_payload(decode= True)
+                    email_data['body'] = body.decode()
+                    # print(body.decode())
+            self.my_messages.append(email_data)
     
     def display_emails_list(self):
         "Display emails in the inbox tab"
-        my_messages = self.check_for_new_mails()
-        my_messages.reverse()
-        self.mails_list = qtw.QListWidget()
-        if my_messages:
-            for email_data in my_messages:
+        #Get the number of emails before update maillist
+        old_emails_counter = len(self.my_messages)
+        #Check for new messages
+        self.check_for_new_mails()
+        #Get the number of emails before update maillist
+        new_emails_counter = len(self.my_messages)
+
+        if self.my_messages and self.source2display == 0:
+            print('step1')
+            for email_data in self.my_messages:
+                self.source2display = 1 
+                item_title = f"From : {email_data['from'] } \nSubject : {email_data['subject']}\nDate : {email_data['date']}"
+                item = qtw.QListWidgetItem(item_title)
+                self.mails_list.addItem(item)
+        elif new_emails_counter > old_emails_counter and self.source2display == 1:
+            print('step2')
+            for email_data in self.my_messages[old_emails_counter:new_emails_counter]:
                 item_title = f"From : {email_data['from'] } \nSubject : {email_data['subject']}\nDate : {email_data['date']}"
                 item = qtw.QListWidgetItem(item_title)
                 self.mails_list.addItem(item)
         else:
             print("no new emails")
-        return self.mails_list
 
     def refresh_function(self):
         "Function to display to new emails if there are"
-        my_messages = self.check_for_new_mails()
-        my_messages.reverse()
-        if my_messages:
-            for email_data in my_messages:
-                item_title = f"From : {email_data['from'] } \nSubject : {email_data['subject']}\nDate : {email_data['date']}"
-                item = qtw.QListWidgetItem(item_title)
-                self.mails_list.addItem(item)
-            self.inbox_layout.replaceWidget(self.mails_list,self.mails_list)
-        else:
-            print("no new emails")
-        
+        self.right_widget.clear()
+        self.tab3 = self.inbox_tab()
+        self.update_tab()
+        self.btn_inbox_function()
+    
+    def helper_function(self,item):
+        "Method to browse email content"
+        self.current_item = self.mails_list.row(item)
+        self.right_widget.removeTab(4)
+        self.new_read_email_tab = self.read_email_tab()
+        self.right_widget.addTab(self.new_read_email_tab,'')
+        self.btn_read_email()
 
+    def delete_email(self,item):
+        "Delete emails"
+        # self.tab3 = self.inbox_tab()
+        # print("step1")
+        # print(len(self.selected_items))
+        # if self.selected_items :
+        #     print("step2")
+        #     for item in self.selected_items:
+        #         self.mails_list.removeItemWidget(item)
+        #         self.my_messages.remove(self.mails_list.row(item))
+        #         self.refresh_function()
+       
+    def forward_function(self):
+        pass
+#Tabs
     def new_email_tab(self):
         "Layout for the email tab"
          #create the layout for this tab
@@ -211,6 +254,7 @@ class MainWindow(qtw.QMainWindow):
         #insert_button.clicked.connect('')
         # "From" label
         from_label = qtw.QLabel(f"From:     {self.email_label.text()}")
+        from_label.setTextInteractionFlags(qtc.Qt.TextSelectableByMouse)
         # Receiver and Subject input"
         self.receiver    =  qtw.QLineEdit(self,clearButtonEnabled = 1,placeholderText = "Please input the email address of the receiver here")
         self.subject    =  qtw.QLineEdit(self,clearButtonEnabled = 1,placeholderText = "Please input the subject of the email here")
@@ -243,27 +287,41 @@ class MainWindow(qtw.QMainWindow):
         
     def inbox_tab(self):
         "Layout for the inbox tab"
+        #Create the layout for this tab
         self.inbox_layout = qtw.QVBoxLayout()
         header_layout = qtw.QHBoxLayout()
-        #Create the Refresh and Select Button
+        #Create the Search,Refresh ,Select and Delete Buttons
         refresh_button = qtw.QPushButton("Refresh")
         refresh_button.clicked.connect(self.refresh_function)
+
         search_button = qtw.QPushButton("Search")
         #select_button.clicked.connect(self.search_function)
-        select_button = qtw.QPushButton("Select")
-        #select_button.clicked.connect(self.select_function)
+
+        delete_button = qtw.QPushButton("Delete")
+        delete_button.clicked.connect(self.delete_email)
+        
+       
         self.search =  qtw.QLineEdit(self,clearButtonEnabled = 0,placeholderText = "Search for emails")
+
+        #Adding the widgets to the layout
         header_layout.addWidget(self.search)
         header_layout.addWidget(search_button)
         header_layout.addSpacing(50)
         header_layout.addWidget(refresh_button)
-        header_layout.addWidget(select_button)
+        header_layout.addWidget(delete_button)
         
+        #layout for the header of this tab
         self.inbox_layout.addLayout(header_layout)
         
-        mails_list = self.display_emails_list()
-        self.inbox_layout.addWidget(mails_list)
+        #Check for new emails
+        self.display_emails_list()
+        
+        #Functions for the button
+        self.mails_list.itemDoubleClicked.connect(self.helper_function)
+        self.mails_list.itemClicked.connect(self.delete_email)
+        self.inbox_layout.addWidget(self.mails_list)
 
+        #Main Widget for this tab
         main = qtw.QWidget()
         main.setLayout(self.inbox_layout)
         return main
@@ -277,9 +335,42 @@ class MainWindow(qtw.QMainWindow):
         main.setLayout(main_layout)
         return main
 
+    def read_email_tab(self):
+        "Layout for the read email tab"
+        read_email_layout = qtw.QVBoxLayout()
+       
+        #Create the Forward Button
+        forward_button = qtw.QPushButton("Forward")
+        forward_button.clicked.connect(self.forward_function)
+
+        read_email_layout.addWidget(forward_button)
+
+        if self.my_messages :
+            print(f'inside:{self.current_item}')
+            email_data = self.my_messages[self.current_item]
+            from_text = qtw.QLabel(f'From:  {email_data["from"]}')
+            from_text.setTextInteractionFlags(qtc.Qt.TextSelectableByMouse)
+            subject_text =  qtw.QLabel(f'Subject:    {email_data["subject"]}')
+            subject_text.setTextInteractionFlags(qtc.Qt.TextSelectableByMouse)
+            date_text =  qtw.QLabel(f'Date: {email_data["date"]}')
+            date_text.setTextInteractionFlags(qtc.Qt.TextSelectableByMouse)
+            body_text  =  qtw.QTextBrowser()
+            body_text.setText(email_data['body'])
+
+            read_email_layout.addSpacing(20)
+            read_email_layout.addWidget(from_text)
+            read_email_layout.addWidget(subject_text)
+            read_email_layout.addWidget(date_text)
+            read_email_layout.addSpacing(10)
+            read_email_layout.addWidget(body_text)
+
+        main = qtw.QWidget()
+        main.setLayout(read_email_layout)
+        return main
+
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
-    mw = MainWindow('alkanstephen@gmail.com','MSC102hynix5*ULTRA')
+    mw = MainWindow('alkanstephen@gmail.com','abc1235*ULTRA',)
     sys.exit(app.exec()) 
 
 
--- email_Login.py
+++ email_Login.py
@@ -1,5 +1,7 @@
 import sys
 import smtplib
+import imaplib
+import smtplib
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
@@ -21,15 +23,27 @@ class EmailLoginWindow(qtw.QWidget):
         layout = qtw.QVBoxLayout()
         self.setLayout(layout)
 
+        #IMAP Server for Gmail
+        #Create the server
+        self.host = 'imap.gmail.com'
+        self.server = self.host
+        self.mail  = imaplib.IMAP4_SSL(self.server)
+
+        #IMAP Server for QQ
+        #Create the server
+        self.mail_qq  = imaplib.IMAP4_SSL('imap.qq.com')
+
+        #flag to open main window
+        self.verif_email = False
+        self.verif_password = False
+        self.login_IMAP = False
+        self.login_SMTP = False
+
         #存储邮件账户与密码的变量
         self.input_email    =  qtw.QLineEdit(self,clearButtonEnabled = 1,placeholderText = "Please input your email here")
         #self.input_password =  qtw.QLineEdit(self,clearButtonEnabled = 1,placeholderText = "Please input your password here").setEchoMode(qtw.QLineEdit.Password)
         self.input_password = PasswordEdit(self,clearButtonEnabled = 1,placeholderText = "Please input your password here" )
         
-        #flag to open main window
-        self.verif_email = False
-        self.verif_password = False
-        #self.openMainwindow = False
 
         #Create layout form for this window 
         form_layout = qtw.QFormLayout()
@@ -46,22 +60,74 @@ class EmailLoginWindow(qtw.QWidget):
         form_layout.addRow(qtw.QLabel('',alignment = 5))
         form_layout.addRow('Email:',self.input_email)
         form_layout.addRow('Password:',self.input_password)
-        form_layout.addRow(qtw.QLabel('',alignment = 5))
+        #form_layout.addRow(qtw.QLabel('',alignment = 5))
         
+        self.qq = qtw.QRadioButton("QQMail")
+        self.gmail = qtw.QRadioButton("GMail")
+
+        layout.addWidget(self.qq)        
+        layout.addWidget(self.gmail)
+
         #Create the next button
-        next_button = qtw.QPushButton("Next",checkable = False)
+        next_button = qtw.QPushButton("Login",checkable = False)
         layout.addWidget(next_button,alignment=qtc.Qt.AlignCenter)
         next_button.clicked.connect(self.test_input_values)
 
         self.show()
 
+    def login_in_IMAP_func(self):
+        "Test if user has been logging in"
+    
+        try:
+            if self.gmail.isChecked():
+                if (self.login_IMAP == False):
+                    self.mail.login(self.input_email.text(),self.input_password.text())
+                    self.login_IMAP = True
+            elif self.qq.isChecked():
+                if (self.login_IMAP == False):
+                    self.mail_qq.login(self.input_email.text(),self.input_password.text())
+                    self.login_IMAP = True    
+            else:
+                qtw.QMessageBox.warning(self,'Warning','Please an email provider')
+        except Exception as error:
+            qtw.QMessageBox.warning(self,'Error Login',f'{str(error)}\nPlease be sure your email and password are corrrect')
+            self.login_IMAP= False
+
+    def login_in_SMPT_func(self):
+        "Test if user has been logging in"
+        try:
+            if self.gmail.isChecked():
+                if (self.login_SMTP == False):
+                    self.server_SMPTP = smtplib.SMTP('smtp.gmail.com',587)#'smtp.gmail.com',587
+                    self.server_SMPTP.starttls()
+                    self.server_SMPTP.login(self.input_email.text(),self.input_password.text())
+                    self.login_SMTP = True
+            elif self.qq.isChecked():
+                 if (self.login_IMAP == False):
+                    self.server_SMPTP = smtplib.SMTP_SSL('smtp.qq.com', smtplib.SMTP_SSL_PORT)
+                    self.server_SMPTP.starttls()
+                    self.server_SMPTP.login(self.input_email.text(),self.input_password.text())
+                    self.login_SMTP = True
+                
+            else:
+                qtw.QMessageBox.warning(self,'Warning','Please an email provider')
+
+        except Exception as error:
+            qtw.QMessageBox.warning(self,'Error Login',f'{str(error)}\nPlease be sure your email and password are corrrect')
+            self.login_SMTP = False
+
+
     def openMainWindow(self):
         "Opens MainWindow"
-        if(self.verif_email  and self.verif_password):
-            self.mw = MainWindow(self.input_email.text(),self.input_password.text())
-            self.close()
-            self.mw.show()
-            #return self.mw
+        if(self.verif_email  and self.verif_password ):
+            self.login_in_IMAP_func()
+            self.login_in_SMPT_func()
+            print(self.login_SMTP)
+            print(self.login_SMTP)
+            if self.login_SMTP and self.login_IMAP :
+                self.mw = MainWindow(self.input_email.text(),self.input_password.text(),self.mail,self.server_SMPTP)
+                self.close()
+                self.mw.show()
         else:
             pass
         
